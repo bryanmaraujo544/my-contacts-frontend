03542d7fcc031ade833457a702b75298192ab415
Bryan Martins <bryanmaraujo544@gmail.com>
2021-12-22 22:12:14 -0300

    feat: adding the component loader

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,13 +6,14 @@ import {
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
-import Modal from '../../components/Modal';
+import Loader from '../../components/Loader';
+// import Modal from '../../components/Modal';
 
 export default function Home() {
   return (
     <Container>
-      <Modal isDanger />
-
+      {/* <Modal isDanger /> */}
+      <Loader />
       <InputSearchContainer>
         <input placeholder="Pesquisar contato" />
       </InputSearchContainer>
