b0dc2107151595d66f9ea016f0dd0e983142d77e
Bryan Martins <bryanmaraujo544@gmail.com>
2021-12-24 10:52:44 -0300

    feat: grabbing the value of the inputs of new contact form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import PropTypes from 'prop-types';
 import { Form, ButtonContainer } from './styles';
 import FormGroup from '../FormGroup';
@@ -7,22 +9,56 @@ import Select from '../Select';
 import Button from '../Button';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log({
+      name, email, phone, category,
+    });
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
-        <Input placeholder="Nome" />
+        <Input
+          placeholder="Nome"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </FormGroup>
+
       <FormGroup
         error="O formato do e-mail é inválido"
       >
-        <Input placeholder="E-mail" error />
+        <Input
+          error
+          placeholder="E-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </FormGroup>
+
       <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
       </FormGroup>
+
       <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="">Categoria</option>
           <option value="intagram">Instagram</option>
+          <option value="discord">Discord</option>
         </Select>
       </FormGroup>
 
