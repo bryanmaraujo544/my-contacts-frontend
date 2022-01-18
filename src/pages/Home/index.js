bd3a458a110a18ea833d6a098fb030de7edfea95
Bryan Martins <bryanmaraujo544@gmail.com>
2022-01-18 12:04:45 -0300

    feat: listing the cards and opening the edit page with the contact id in the url

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import {
   Container, Header, ListContainer, Card, InputSearchContainer,
 } from './styles';
@@ -9,7 +10,24 @@ import trash from '../../assets/images/icons/trash.svg';
 // import Loader from '../../components/Loader';
 // import Modal from '../../components/Modal';
 
+import formatPhone from '../../utils/formatPhone';
+
 export default function Home() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/contacts')
+      .then(async (response) => {
+        const contactsJson = await response.json();
+        setContacts(contactsJson);
+      })
+      .catch((error) => {
+        console.log('error', error);
+      });
+  }, []);
+
+  console.log(contacts);
+
   return (
     <Container>
       {/* <Modal isDanger /> */}
@@ -18,7 +36,11 @@ export default function Home() {
         <input placeholder="Pesquisar contato" />
       </InputSearchContainer>
       <Header>
-        <strong>3 contatos</strong>
+        <strong>
+          {contacts.length}
+          {' '}
+          {contacts.length === 1 ? 'contato' : 'contatos'}
+        </strong>
         <Link to="/new">Novo contato</Link>
       </Header>
 
@@ -29,34 +51,29 @@ export default function Home() {
             <img src={arrow} alt="top-arrowl" />
           </button>
         </header>
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Mateus Silva</strong>
-              <small>instragram</small>
+        {contacts.map((contact) => (
+          <Card>
+            <div className="info">
+              <div className="contact-name">
+                <strong>{contact.name}</strong>
+                {contact.category_name && <small>{contact.category_name}</small>}
+              </div>
+              <span>{contact.email}</span>
+              <span>
+                {formatPhone(contact.phone)}
+              </span>
             </div>
-            <span>mateus@devacademy.com.br</span>
-            <span>(11) 99999-9999</span>
-          </div>
-          <div className="actions">
-            <Link to="/edit/123">
-              <img src={edit} alt="Edit" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </Card>
+            <div className="actions">
+              <Link to={`/edit/${contact.id}`}>
+                <img src={edit} alt="Edit" />
+              </Link>
+              <button type="button">
+                <img src={trash} alt="Delete" />
+              </button>
+            </div>
+          </Card>
+        ))}
       </ListContainer>
     </Container>
   );
 }
-
-fetch('http://localhost:3001/contacts')
-  .then(async (response) => {
-    const json = await response.json();
-    console.log({ json });
-  })
-  .catch((error) => {
-    console.log('error', error);
-  });
